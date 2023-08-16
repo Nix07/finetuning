8de3764fcb2e6896fe752565c21bfc934e20c890
Tamar Rott Shaham <tamarott@visiongpu39.csail.mit.edu>
2023-08-15 23:52:51 -0400

    update dataset format

--- arithmetics_dataset/generate_dataset.py
+++ arithmetics_dataset/generate_dataset.py
@@ -5,12 +5,20 @@ import string
 n_var = 3 # number of variables
 n_prompts = 600 # number of statements
 
-def generate_random_data(n_var):
-    variables = random.sample(string.ascii_lowercase, n_var)
-    values = random.sample(range(100), n_var)
-    q = random.randint(0, len(variables) - 1)
-    q_value = values[q]
-    q_var = variables[q]
+def generate_random_data(n_var,existing_data):
+    while True:
+        variables = random.sample(string.ascii_lowercase, n_var)
+        values = random.sample(range(100), n_var)
+        q = random.randint(0, len(variables) - 1)
+        q_value = values[q]
+        q_var = variables[q]
+        data = {f"var_{i}": var for i, var in enumerate(variables)}
+        data.update({f"val_{i}": val for i, val in enumerate(values)})
+        frozen_data = frozenset(data.items())  # Convert the dictionary to a frozenset
+        if not (frozen_data in existing_data):  # Add the new data to the set
+            break
+
+    existing_data.add(frozen_data)    
     prompt = ''
     for i,(var,val) in enumerate(zip(variables,values)):
         if i > 0:
@@ -18,16 +26,18 @@ def generate_random_data(n_var):
         else:
             prompt += f"{var}={val}"
     prompt += f". {q_var}={q_value}."
-    return "{"+prompt+"}\n"
+    return prompt
 
 def main(n_var,n_prompts):
-    json_data = ''
+    json_data = []
+    existing_data = set()
     for _ in range(n_prompts):
-        data = generate_random_data(n_var)
-        json_data += data
+        prompt = generate_random_data(n_var,existing_data)
+        json_data.append({"sentence": prompt})
 
     with open(f'{n_var}.json', 'w') as json_file:
-        json.dump(json_data, json_file, indent=4)
-
+        for data in json_data:
+            json.dump(data, json_file)
+            json_file.write('\n')
 if __name__ == "__main__":
     main(n_var,n_prompts)
