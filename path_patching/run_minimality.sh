f6cb908bdaae66a4793524318a27c35a456e4ae8
Nikhil Prakash <nikhil07prakash@gmail.com>
2024-01-04 13:04:21 -0500

    More deletion

--- path_patching/run_minimality.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source ~/.bashrc
-cd ../
-conda activate anima
-python "/data/nikhil_prakash/anima-2.0/path_patching/minimality.py" --datafile="/data/nikhil_prakash/anima-2.0/box_datasets/no_instructions/alternative/Random/7/train.jsonl" --circuit_root_path="/data/nikhil_prakash/anima-2.0/path_patching/Sat_Dec__9_15:16:21_2023/path_patching_results/20" --model_name="llama" --n_value_fetcher=58 --n_pos_trans=10 --n_pos_detect=25 --n_struct_read=5 --num_samples=100 --batch_size=100 --results_path="/data/nikhil_prakash/anima-2.0/path_patching/results/minimality_results"
